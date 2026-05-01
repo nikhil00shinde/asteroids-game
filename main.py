8943a3eea90e676f7245b0806b6800e037014f03
nikhil00shinde <nikhilshinde00@outlook.com>
2026-05-01 18:39:44 -0400

    added: till gameloop

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import pygame
+from constants import *
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
